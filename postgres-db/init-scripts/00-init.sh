241c4eb21c081aa70c3955d92c6b177ed3fb5d7b
Rabindra Joshi <mail@rjoshi.net>
2020-04-09 00:08:36 +0545

    Start tracking, git!

---
+++ postgres-db/init-scripts/00-init.sh
@@ -0,0 +1,1 @@
+usermod -u $UID postgres
---
+++ postgres-db/init-scripts/01-init.sql
@@ -0,0 +1,8 @@
+CREATE TABLE data (
+  ID SERIAL PRIMARY KEY,
+  firstname TEXT,
+  lastname TEXT
+);
+INSERT INTO data (firstname, lastname)
+VALUES
+  ('John', 'Doe');
